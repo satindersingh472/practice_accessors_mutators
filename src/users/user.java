5d778a88fc897ba8d32cf55957ef5feb2d9edfa1
satindersingh772 <92008199+satindersingh772@users.noreply.github.com>
2023-01-09 22:02:20 -0700

    finished

--- src/users/user.java
+++ src/users/user.java
@@ -1,11 +1,10 @@
 package users;
 
 public class user{
-   
-    public static boolean satinder;
+
     String user_name;
     int age;
-    boolean is_subscribed;
+     boolean is_subscribed;
     String dob;
     boolean attending_event;
     
@@ -16,12 +15,21 @@ public class user{
         dob = birth_input;
         attending_event = attend_event;
     }
+
+    public void comingToEvent(){
+        if(this.is_subscribed){
+            System.out.println(" is subscribed");
+        }
+    }
     public static void main(String[] args){
         user satinder = new user("satinder",28,true,"1994-11-15",true);
         user sam = new user("Sam", 26, false, "1996-07-24",false);
         user nuhaar = new user("Nuhaar",2,false,"2021-05-11",true);
         user jag = new user("jag",55,true, "1967-03-06",false);
         user amb = new user("amb", 5, false, "2018-01-16",true);
+
+        satinder.comingToEvent();
+
 
     }
 }
